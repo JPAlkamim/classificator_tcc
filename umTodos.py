298ff56a5fa0d0bf61c79abdfdc609b8ecda3822
JPAlkamim <jpalkamim@gmail.com>
2023-03-11 17:05:52 -0300

    adicionei um pra todos

--- umTodos.py
+++ umTodos.py
@@ -8,10 +8,15 @@ from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
 
-X9 = np.load('Banco/X9.npy')
-y9 = np.load('Banco/y9.npy')
+X = np.load('Banco/X9.npy')
+y = np.load('Banco/y9.npy')
 
-X_train, X_test, y_train, y_test = train_test_split(X3, y3, test_size=0.3, random_state=42)
+X_4 = X[y == 4]
+y_4 = y[y == 4]
+
+y_4_vs_all = np.where(y == 4, 1, 0)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y_4_vs_all, test_size=0.3, random_state=42)
 
 knn = KNeighborsClassifier()
 parametros = {'n_neighbors': [3, 5, 7, 9, 11]}
